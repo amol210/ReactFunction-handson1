80e5639700f59488272c9383ee13b60a2623bd3b
amol210 <114400251+amol210@users.noreply.github.com>
2023-03-30 12:21:03 +0530

    src
    src

---
+++ src/component/FunctionCompo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+
+const FunctionCompo = () => {
+  return (
+    <>
+    <div className="box" id="box1">
+        <h2>This is created using Function Components.</h2>
+        <p>This is Done by using External CSS.</p>
+        <p className="line3">This is Done by using Inline CSS.</p>
+    </div>
+     </>
+  )
+}
+
+export default FunctionCompo
